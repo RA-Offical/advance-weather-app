626b6c26cd2c4157b3340db7d7115f9c178156a7
Rashid Ameer <r.ameer.offical@gmail.com>
2023-08-22 11:43:55 +0500

    Make env variables

--- src/data/url.js
+++ src/data/url.js
@@ -1,17 +1,24 @@
 const baseURL = "https://api.openweathermap.org/data/2.5";
-const APIKEY = "157cc963625396049b1ef33b0a6b98a4";
 
 const URL = {
 	getWeatherByCoordinates: (lat, lon) =>
-		`${baseURL}/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`,
+		`${baseURL}/weather?lat=${lat}&lon=${lon}&appid=${
+			import.meta.env.VITE_API_KEY
+		}`,
 	getWeatherByQuery: (query) =>
-		`${baseURL}/weather?q=${query}&appid=${APIKEY}`,
+		`${baseURL}/weather?q=${query}&appid=${import.meta.env.VITE_API_KEY}`,
 	getForecastByCoordinates: (lat, lon) =>
-		`${baseURL}/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}`,
+		`${baseURL}/forecast?lat=${lat}&lon=${lon}&appid=${
+			import.meta.env.VITE_API_KEY
+		}`,
 	getAirPollutionByCoordinates: (lat, lon) =>
-		`${baseURL}/air_pollution?lat=${lat}&lon=${lon}&appid=${APIKEY}`,
+		`${baseURL}/air_pollution?lat=${lat}&lon=${lon}&appid=${
+			import.meta.env.VITE_API_KEY
+		}`,
 	getHintList: (query) =>
-		`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${APIKEY}`,
+		`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${
+			import.meta.env.VITE_API_KEY
+		}`,
 };
 
 export default URL;
